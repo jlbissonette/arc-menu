1d8da675ef6240c7dac93b7c814cd796a4011c07
Andrew Zaech <4151635-AndrewZaech@users.noreply.gitlab.com>
2021-11-03 20:31:44 +0000

    Tooltips: Fix non-translatable strings
    -Fixes https://gitlab.com/arcmenu/ArcMenu/-/issues/143

--- menuWidgets.js
+++ menuWidgets.js
@@ -856,7 +856,7 @@ var Tooltip = class Arc_Menu_Tooltip{
       
         if(title){
             this.titleLabel = new St.Label({
-                text: title,
+                text: _(title),
                 style: description ? "font-weight: bold;" : null,
                 y_align: Clutter.ActorAlign.CENTER
             });
